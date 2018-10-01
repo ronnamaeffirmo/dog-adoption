0b9337c74c25439c9e5ffe3ea9f84bc8a9d0ca38
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-01 22:45:12 +0800

    add checking if dog is already adopted

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,4 +1,13 @@
 async function adoptDog(root, args, context, info) {
+	const dogAdopted = await context.db.exists.Dog({
+		id: args.id,
+		status: 'ADOPTED'
+	})
+
+	if (dogAdopted) {
+		throw new Error(`Dog already adopted! ID: ${args.id}`)
+	}
+
 	const dog =  await context.db.mutation.updateDog({
 		where: { id: args.id },
 		data: { status: 'ADOPTED' }
